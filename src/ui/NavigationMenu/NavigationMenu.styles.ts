3468c1ee30aadf06b85a0ef64e73d2bd3e45a700
K3NXXX <xxvolodyax7@gmail.com>
2025-03-14 19:09:34 +0200

    added photo adding ways ui

--- src/ui/NavigationMenu/NavigationMenu.styles.ts
+++ src/ui/NavigationMenu/NavigationMenu.styles.ts
@@ -31,6 +31,34 @@ export const styles = StyleSheet.create({
 		width: 50,
 		height: 50,
 		
+	},
+	addPhotoWrapper: { position: "relative"},
+	ways: {
+		position: "absolute",
+		alignItems: "center",
+		width: 230,
+		top: -55,
+		left: -45,
+		backgroundColor: "#ee9938",
+		borderRadius: 20
+	},
+	way: {
+		fontFamily: "inter-bold",
+		fontSize: 17,
+		marginVertical: 8,
+		color: "white"
+	},
+	tail: {
+		position: "absolute",
+		bottom: -20,
+		width: 0,
+		height: 0,
+		borderLeftWidth: 10,
+		borderRightWidth: 10,
+		borderTopWidth: 20,
+		borderLeftColor: 'transparent',
+		borderRightColor: 'transparent',
+		borderTopColor: '#ee9938',
 	}
 
 	
--- src/ui/NavigationMenu/NavigationMenu.tsx
+++ src/ui/NavigationMenu/NavigationMenu.tsx
@@ -1,18 +1,35 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React, { useState } from 'react'
+import { Text, TouchableOpacity, View } from 'react-native'
 import { menuList } from '../../lists/menu.list'
 import { styles } from './NavigationMenu.styles'
 
 const NavigationMenu: React.FC = () => {
+	const [addPhoto, setAddPhoto] = useState(false)
+
 	return (
 		<View style={styles.root}>
 			<View style={styles.wrapper}>
 				{menuList.map(item => (
 					<View style={styles.item} key={item.id}>
-						<item.icon
-							style={item.id === 3 ? styles.createIcon : styles.icon}
-							{...(item.id === 3 ? { fill: '#ee9938' } : {})} 
-						/>
+						{item.id === 3 ? (
+							<View style={styles.addPhotoWrapper}>
+								{addPhoto && (
+									<View style={styles.ways}>
+										<View style={styles.ways}>
+											<Text style={styles.way}>Create photo</Text>
+											<Text style={styles.way}>Add photo from gallery</Text>
+											<View style={styles.tail} />
+										</View>
+									</View>
+								)} 
+
+								<TouchableOpacity onPress={() => setAddPhoto(!addPhoto)}>
+									<item.icon style={styles.createIcon} fill='#ee9938' />
+								</TouchableOpacity>
+							</View>
+						) : (
+							<item.icon style={styles.icon} />
+						)}
 						<Text style={styles.label}>{item.label}</Text>
 					</View>
 				))}
