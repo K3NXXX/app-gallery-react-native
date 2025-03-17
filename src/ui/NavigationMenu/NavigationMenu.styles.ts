3d3f0b73b27611d97f9b6b34dbaadd5659c50cf4
K3NXXX <xxvolodyax7@gmail.com>
2025-03-17 11:56:02 +0200

    added ui for adding photo

--- src/ui/NavigationMenu/NavigationMenu.styles.ts
+++ src/ui/NavigationMenu/NavigationMenu.styles.ts
@@ -2,24 +2,21 @@ import { StyleSheet } from 'react-native'
 
 export const styles = StyleSheet.create({
 	root: {
-		position: 'absolute', 
+		position: 'absolute',
 		left: 0,
 		right: 0,
-		bottom: 0, 
-		paddingBottom: 40,  
-	  },
+		bottom: 0,
+		paddingBottom: 40,
+	},
 	wrapper: {
 		flexDirection: "row",
 		justifyContent: "center"
-		
-		
 	},
 	item: {
 		alignItems: "center",
 		width: "20%"
-
 	},
-	icon :{
+	icon: {
 		width: 25,
 		height: 25,
 		marginBottom: 6
@@ -30,37 +27,64 @@ export const styles = StyleSheet.create({
 	createIcon: {
 		width: 50,
 		height: 50,
-		
 	},
-	addPhotoWrapper: { position: "relative"},
-	ways: {
-		position: "absolute",
+	addPhotoWrapper: {
+		position: "absolute", 
+		width: "100%",
+		height: "100%",
+		top: 0,
+		left: 0,
+		right: 0,
+		bottom: 0,
+		justifyContent: "center",
 		alignItems: "center",
-		width: 230,
-		top: -55,
-		left: -45,
-		backgroundColor: "#ee9938",
-		borderRadius: 20
 	},
-	way: {
-		fontFamily: "inter-bold",
-		fontSize: 17,
-		marginVertical: 8,
-		color: "white"
-	},
-	tail: {
+	modal: {
 		position: "absolute",
-		bottom: -20,
-		width: 0,
-		height: 0,
-		borderLeftWidth: 10,
-		borderRightWidth: 10,
-		borderTopWidth: 20,
-		borderLeftColor: 'transparent',
-		borderRightColor: 'transparent',
-		borderTopColor: '#ee9938',
-	}
+		width: "100%",
+		height: "100%",
+		backgroundColor: "rgba(0, 0, 0, 0.4)", 
+		justifyContent: "center",
+		alignItems: "center",
+		zIndex: 100,
+	},
+	ways: {
+		backgroundColor: "white",
+		padding: 25,
+		paddingHorizontal: 40,
+		borderRadius: 20,
+		alignItems: 'center'
+	},
+	title:{
+		fontFamily: "inter-bold",
+		fontSize: 25,
+		color: "black",
+		marginBottom: 20
 
+	},
 	
+	way: {
+		fontFamily: "inter-regular",
+		fontSize: 17,
+		color: "#000000",
 	
+	},
+	wayWrapper: {
+		flexDirection: "row",
+	},
+	container: {
+		alignItems: "center",
+		backgroundColor: "#dfe8ef",
+		borderRadius: 20,
+		paddingVertical: 10,
+		paddingHorizontal: 15,
+
+	},
+	firstContainer: {
+		marginRight: 15, 
+
+	},
+	iconGallery: {
+		marginVertical: 5
+	},
 })
--- src/ui/NavigationMenu/NavigationMenu.tsx
+++ src/ui/NavigationMenu/NavigationMenu.tsx
@@ -1,41 +1,59 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import { Text, TouchableOpacity, View } from 'react-native'
 import { menuList } from '../../lists/menu.list'
 import { styles } from './NavigationMenu.styles'
+import CameraIcon from "../../../assets/images/navigation-menu/camera.svg"
+import GalleryIcon from "../../../assets/images/navigation-menu/gallery-icon.svg"
+import { useOnClickOutside } from 'usehooks-ts'
 
 const NavigationMenu: React.FC = () => {
 	const [addPhoto, setAddPhoto] = useState(false)
+	const ref = useRef(null)
 
+	
+	const handleClickOutside = () => {
+		setAddPhoto(false)
+	  }
+	useOnClickOutside(ref, handleClickOutside)
 	return (
-		<View style={styles.root}>
-			<View style={styles.wrapper}>
-				{menuList.map(item => (
-					<View style={styles.item} key={item.id}>
-						{item.id === 3 ? (
-							<View style={styles.addPhotoWrapper}>
-								{addPhoto && (
-									<View style={styles.ways}>
-										<View style={styles.ways}>
-											<Text style={styles.way}>Create photo</Text>
-											<Text style={styles.way}>Add photo from gallery</Text>
-											<View style={styles.tail} />
-										</View>
-									</View>
-								)} 
-
-								<TouchableOpacity onPress={() => setAddPhoto(!addPhoto)}>
-									<item.icon style={styles.createIcon} fill='#ee9938' />
-								</TouchableOpacity>
-							</View>
-						) : (
-							<item.icon style={styles.icon} />
-						)}
-						<Text style={styles.label}>{item.label}</Text>
+		<>
+			{addPhoto && (
+			<View style={styles.modal}>
+				<View  ref={ref} style={styles.ways}>
+					<Text style={styles.title}>Add photo</Text>
+					<View style={styles.wayWrapper}>
+					<View style={[styles.container, styles.firstContainer]}>
+							<CameraIcon width={40} height={40}/>
+							<Text style={styles.way}>Camera</Text>
+						</View>
+						<View style={styles.container}>
+							<GalleryIcon style={styles.iconGallery} width={30} height={30}/>
+							<Text style={styles.way}>Gallery</Text>
+						</View>
 					</View>
-				))}
+				</View>
 			</View>
-		</View>
+			)}
+
+			<View style={styles.root}>
+				<View style={styles.wrapper}>
+					{menuList.map(item => (
+						<View style={styles.item} key={item.id}>
+							{item.id === 3 ? (
+								<View style={styles.addPhotoWrapper}>
+									<TouchableOpacity onPress={() => setAddPhoto(!addPhoto)}>
+										<item.icon style={styles.createIcon} fill='#ee9938' />
+									</TouchableOpacity>
+								</View>
+							) : (
+								<item.icon style={styles.icon} />
+							)}
+							<Text style={styles.label}>{item.label}</Text>
+						</View>
+					))}
+				</View>
+			</View>
+		</>
 	)
 }
-
 export default NavigationMenu
